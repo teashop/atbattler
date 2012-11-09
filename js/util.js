cbecc1750ea0c7c95dd3f952b44e5cc29a671c18
teashop <mobileteashop@gmail.com>
2012-11-09 03:51:08 +0000

    Moved EventQueue to util.js

--- js/util.js
+++ js/util.js
@@ -1,4 +1,8 @@
-/** Clock **/
+/**
+ * A simple, event-firing clock with a configurable tick size.
+ * The Clock uses JavaScript setTimeout with self-adjustment to ensure that the
+ * ticks are not skewed by variance in tge timer.
+ */
 function Clock(tickSize) {
 	this.tickSize = tickSize;
 	this.prevTime = (new Date()).getTime();
@@ -36,4 +40,25 @@ C.start = function(adjustment) {
 	this.timer = setTimeout(this.nextTick.bind(this), adjustment ? this.tickSize - adjustment : this.tickSize);
 }
 /** END Clock **/
+
+
+/**
+ * A simple representation of an 'event-queue', which fires a 'queued' event
+ * when a new event is added.
+ */
+function EventQueue() {
+	this.events = [];
+}
+var EQ = EventQueue.prototype;
+EQ.push = function(theEvent) {
+	this.events.push(theEvent);
+	$(this).trigger('queued');
+}
+EQ.shift = function() {
+	return this.events.shift();
+}
+EQ.clear = function() {
+	this.events = [];
+}
+/** END Event Queue */
 
